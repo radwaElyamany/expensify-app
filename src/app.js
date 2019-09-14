96e67b7189cfb8d1209784a4322801d4b051ef82
Radwa-ElYamany <radwa.elyamany@vodafone.com>
2019-09-14 04:10:52 +0200

    setup production webpack build

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import "normalize.css/normalize.css";
-import "./styles/styles.scss";
 import AppRouter from "./routes/AppRouter";
 import configureStore from "./store/configureStore";
+import "normalize.css/normalize.css";
+import "./styles/styles.scss";
 import "react-dates/lib/css/_datepicker.css";
 
 const store = configureStore();
