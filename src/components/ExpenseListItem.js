3367a2ea0b93dd11ffc43b7de80a850f75206c07
Radwa-ElYamany <radwa.elyamany@vodafone.com>
2019-09-14 13:10:46 +0200

    setup fromating for amount and createdAt

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import moment from "moment";
+import numeral from "numeral";
+
 import { removeExpense } from "../actions/expenses";
 
 const ExpenseListItem = ({ id, description, amount, createdAt }) => (
   <div>
-  <Link to={`/edit/${id}`}>
-    <h3>{description}</h3>
-  </Link>
+    <Link to={`/edit/${id}`}>
+      <h3>{description}</h3>
+    </Link>
     <p>
-      {amount} - {createdAt}
+      {numeral(amount / 100).format("$0,0.00")} -{" "}
+      {moment(createdAt).format("MMMM Do, YYYY")}
     </p>
   </div>
 );
